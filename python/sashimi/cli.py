920af6f2d378c1c83d5ab7dd759a5e82a5983670
Peanut-XIV <64585726+Peanut-XIV@users.noreply.github.com>
2023-05-24 17:16:02 +0200

    Update cli.py

--- python/sashimi/cli.py
+++ python/sashimi/cli.py
@@ -15,42 +15,42 @@ def cli():
 
 
 @cli.command()
-@click.argument(dcls=[None, '--dir', '-d', 'dir_'],
-                type=str,
-                default=path.expanduser('~/images/sashimi/'),
-                prompt='Directory to save images',
-                help='Directory to save images')
-@click.argument(dcls=[None, '--port', '-p'],
-                type=str,
-                default='COM5',
-                prompt='COM port of printer',
-                help='COM port of the 3D printer')
-@click.option(dcls=['--lang', '-l'],
+@click.option('--dir', '-d', 'dir_',
+              type=str,
+              prompt='Directory to save images',
+              default=path.expanduser('~/images/sashimi/'),
+              help='Directory to save images')
+@click.option('--port', '-p',
+              type=str,
+              prompt='COM port of printer',
+              default='COM5',
+              help='COM port of the 3D printer')
+@click.option('--lang', '-l',
               type=str,
               default="en",
-              prompt="Language",
+              # prompt="Language",
               help='Language of the interface (en/fr)')
 @click.option('--layout',
               type=str,
               default='QWERTY',
               help='Layout of the keyboard (QWERTY/AZERTY)')
-@click.option(dcls=["--mult-exp", "-e"],
+@click.option("--mult-exp", "-e",
               is_flag=True,
               help="Allows to use multiple Exposures")
-@click.option(dcls=["--remove-pics", "-r"],
+@click.option("--remove-pics", "-r",
               is_flag=True,
               help='Removes the non-stacked pictures after finishing stacking')
-@click.option(dcls=["--skip-fs", "-s"],
+@click.option("--skip-fs", "-s",
               is_flag=True,
               help='disable the automatic focus-stacking of pictures after a scan')
-@click.option(dcls=['--auto-quit', '-q'],
+@click.option('--auto-quit', '-q',
               is_flag=True,
               help='sashimi quits automatically after scanning')
-@click.option(dcls=['--offset', '-o'],
+@click.option('--offset', '-o',
               type=int,
               default=1000,
               help='z offset in top-down mode')
-@click.option(dcls=['--lowest', '-z'],
+@click.option('--lowest', '-z',
               is_flag=True,
               help='simplifies z correction')
 def scan(dir_, port, lang, layout, mult_exp, remove_pics, skip_fs, auto_quit, offset, lowest):
@@ -75,18 +75,18 @@ def scan(dir_, port, lang, layout, mult_exp, remove_pics, skip_fs, auto_quit, of
 
 
 @cli.command()
-@click.option(dcls=[None, '--dir', '-d', 'dir_'],
+@click.option('--dir', '-d', 'dir_',
               type=str,
-              prompt='Directory containing stacks',
+              # prompt='Directory containing stacks',
               help='Directory containing subdirectories of image stacks')
 def stack(dir_):
     focus_stack.stack(dir_)
 
 
 @cli.command()
-@click.option(dcls=[None, '--dir', '-d', 'dir_'],
+@click.option('--dir', '-d', 'dir_',
               type=str,
-              prompt='Directory containing stacks',
+              # prompt='Directory containing stacks',
               help='Directory containing subdirectories of image stacks')
 def helicon_stack(dir_):
     helicon_stacker.stack(dir_)
