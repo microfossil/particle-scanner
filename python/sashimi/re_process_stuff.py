ff3170ed910717de053b5064a81cbd89dc394665
Peanut-XIV <64585726+Peanut-XIV@users.noreply.github.com>
2023-06-26 19:32:43 +0200

    Delete re_process_stuff.py

--- python/sashimi/re_process_stuff.py
+++
@@ -1,7 +0,0 @@
-from sashimi.helicon_stack import stack_for_multiple_exp
-from pathlib import Path
-
-from_ = Path(input("path from: "))
-to_ = Path(input("Path to: "))
-stack_for_multiple_exp(from_, to_, [1400])
-
