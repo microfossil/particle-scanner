0f3c50bdcfe01e01aec1138bf002f7cb5827019e
Peanut-XIV <64585726+Peanut-XIV@users.noreply.github.com>
2023-06-30 15:17:30 +0200

    edited main

--- python/Sashimi/sashimi/main.py
+++ python/Sashimi/sashimi/main.py
@@ -3,16 +3,16 @@ from pathlib import Path
 import os
 import shutil
 
+if __name__ == "__main__":
+    my_dir = Path("C:/Users/utilisateur.SF_GEOL_STAG_IN/Desktop/Data/dump")
+    os.makedirs(my_dir, exist_ok=True)
 
-my_dir = Path("C:/Users/utilisateur.SF_GEOL_STAG_IN/Desktop/Data/dump")
-os.makedirs(my_dir, exist_ok=True)
+    for files in os.listdir(my_dir):
+        path = os.path.join(my_dir, files)
+        try:
+            shutil.rmtree(path)
+        except OSError:
+            os.remove(path)
 
-for files in os.listdir(my_dir):
-    path = os.path.join(my_dir, files)
-    try:
-        shutil.rmtree(path)
-    except OSError:
-        os.remove(path)
-
-controller = Controller(my_dir, "COM5", lang="en", layout='AZERTY', auto_f_stack=True, lowest_z=True)
-controller.start()
+    controller = Controller(my_dir, "COM5", lang="en", layout='AZERTY', auto_f_stack=True, lowest_z=True)
+    controller.start()
