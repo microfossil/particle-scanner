3bafb68d7263983b4747208d60a36065661a374e
Anthogr <87010226+Anthogr@users.noreply.github.com>
2025-03-21 15:21:10 +0100

    Added check position + correction_factor computation

--- python/Sashimi/sashimi/stage.py
+++ python/Sashimi/sashimi/stage.py
@@ -2,6 +2,11 @@ import cv2
 import serial
 import time
 import requests
+from decimal import Decimal, getcontext
+
+# Set precision for correction factor computation
+getcontext().prec = 10
+
 class Stage(object):
     def __init__(self, controller, printer_ip, port):
         self.controller = controller
@@ -17,7 +22,7 @@ class Stage(object):
         self.reported_x = 0
         self.reported_y = 0
         self.reported_z = 0
-
+        
         self.x_limits = (0, 200000)
         self.y_limits = (0, 200000)
         self.z_limits = (0, 20000)
@@ -36,6 +41,10 @@ class Stage(object):
         self.goto_x(home_position[0])
         self.goto_y(home_position[1])
         self.goto_z(home_position[2])
+
+        if self.check_position_reached(home_position[0], home_position[1], home_position[2]):
+            print("Printer reached position X:" \
+                  f"{home_position[0]}µm Y:{home_position[1]}µm Z:{home_position[2]}µm")
 
     def move_x(self, distance_um):
         self.goto_x(self.x + distance_um)
@@ -77,6 +86,45 @@ class Stage(object):
         self.goto_y(position[1])
         self.goto_z(position[2])
 
+    def correction_factor_compute(self):
+        """
+        There is currently an offset between Z position sent to the 3D printer 
+        and Z position returned from it. 
+            e.g. When Requesting printer to get to Z pos. at 2mm
+              field "position" from printer object "gcode_move" will return 1.9944mm
+        This offset is linearly increasing with absolute Z position
+            i.e. When Requesting printer to get to Z at 1mm, offset will be 1 x 0.0028mm, requesting
+                 it to get to Z at 2mm, offset will be 2 x 0.0028mm, etc.
+        This offset might be due to the precision and accuracy limitations of the printer's hardware
+        and firmware. 
+        We compensate this difference by introducing correction_factor variable. It is computed 
+        as the difference between the Z position the printer is moved to (1mm) and the position read 
+        from it.
+        """
+        self.send_gcode("G0 Z 1")
+        response = self.get_query_printer_object({"gcode_move": ["position"]})
+        z_position_read = response['result']['status']['gcode_move']['position'][2]
+        correction_factor = Decimal('1') - Decimal(f'{z_position_read}')
+        print(f"Z correction factor = {correction_factor}")
+        return correction_factor
+
+    def check_position_reached(self, x_target, y_target, z_target):
+        """This function checks if the printer has reached the target position"""
+        response = self.get_query_printer_object({"gcode_move": ["position"]})
+        x_position_read = response['result']['status']['gcode_move']['position'][0]
+        y_position_read = response['result']['status']['gcode_move']['position'][1]
+        z_position_read = response['result']['status']['gcode_move']['position'][2]
+
+        correction_factor = self.correction_factor_compute()
+        offset = float(correction_factor * z_target / 1000)
+
+        if (
+            x_position_read == x_target/1000 and
+            y_position_read == y_target/1000 and
+            z_position_read + offset == z_target/1000
+            ):
+            return True
+        
     def send_gcode(self, command, timeout=60):
         url = f"{self.printer_ip}:{self.port}/printer/gcode/script"
         payload = {"script": command}
